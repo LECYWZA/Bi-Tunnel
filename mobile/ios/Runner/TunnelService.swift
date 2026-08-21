a300cf5ab990c1f94a01da56f098fbbe39bf2df0
luojiecong <1210225958@qq.com>
2026-08-21 12:20:35 +0800

    fix: iOS DispatchQueue.async 显式闭包参数类型规避重载歧义

--- mobile/ios/Runner/TunnelService.swift
+++ mobile/ios/Runner/TunnelService.swift
@@ -394,7 +394,7 @@ class ClientRunner {
 
         let queue: DispatchQueue = DispatchQueue(label: "client-runner-\(id)")
         workerQueue = queue
-        queue.async { [weak self] in
+        queue.async { [weak self] (_: Void) in
             guard let self = self else { return }
             var reconnectAttempt = 0
 
